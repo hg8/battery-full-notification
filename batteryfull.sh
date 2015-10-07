f09d8c633ad0cd03d19c63ef491cf34e14add038
Hugo F <hforestier@live.fr>
2015-10-07 16:20:05 +0200

    small corrections

--- batteryfull.sh
+++ batteryfull.sh
@@ -1,14 +1,12 @@
-#!/bin/bash
-
-    while true
-    do
-        export DISPLAY=:0.0
-        battery_percent=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-        if on_ac_power; then
-            if [ $battery_percent -g 95 ]; then
-                notify-send -i $PWD/batteryfull.png "Battery full." "Level: ${battery_percent}% "
-             fi
+#!/usr/bin/env bash
+while true
+do
+    export DISPLAY=:0.0
+    battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    if on_ac_power; then
+        if [ "$battery_percent" -gt 95 ]; then
+            notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
         fi
-
-        sleep 300 #(5 minutes)
-    done
+    fi
+    sleep 300 # (5 minutes)
+done
