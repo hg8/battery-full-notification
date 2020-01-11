b2e3c9cf0adf29aa835be50ee671932704fb198a
Moataz Mohammady <moataz.mohammady@gmail.com>
2020-01-11 20:57:48 +0200

    print battery_percent

--- batteryfull.sh
+++ batteryfull.sh
@@ -3,6 +3,7 @@ while true
 do
     export DISPLAY=:0.0
     battery_percent=$(acpi -b | grep -P -o '[0-9]+(?=%)')
+    echo "Battery percent: " $battery_percent"%"
     if on_ac_power; then
         if [ "$battery_percent" -gt 95 ]; then
             notify-send -i "$PWD/batteryfull.png" "Battery full." "Level: ${battery_percent}% "
